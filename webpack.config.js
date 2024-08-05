ec6e7e46c3416c9e3f070b64c997be2636075ce6
Richard Essuman <ressuman001@gmail.com>
2024-08-05 09:45:41 +0000

    "Added @types/dotenv-webpack dependency, updated webpack.config.js to include Dotenv plugin, and refactored webpack configuration"

--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,5 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const Dotenv = require("dotenv-webpack");
-
-module.exports = {
-  entry: "./src/ts/app.ts",
-
-  output: {
-    filename: "js/bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-
-  devtool: "inline-source-map",
-
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif|svg)$/i,
-        type: "asset/resource",
-        generator: {
-          filename: "images/[name][ext][query]",
-        },
-      },
-    ],
-  },
-
-  resolve: {
-    extensions: [".ts", ".js"],
-  },
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./index.html",
-      filename: "index.html",
-    }),
-    new Dotenv(),
-  ],
-
-  devServer: {
-    // static: {
-    //   directory: path.resolve(__dirname, "dist"),
-    // },
-    static: [
-      {
-        directory: path.join(__dirname),
-      },
-    ],
-    compress: true,
-    port: 9000,
-    open: true,
-  },
-
-  mode: "development",
-};
-
 // const path = require("path");
 // const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const CleanPlugin = require("clean-webpack-plugin");
 // const Dotenv = require("dotenv-webpack");
 
 // module.exports = {
@@ -75,7 +10,7 @@ module.exports = {
 //     path: path.resolve(__dirname, "dist"),
 //   },
 
-//   devtool: "source-map",
+//   devtool: "inline-source-map",
 
 //   module: {
 //     rules: [
@@ -108,18 +43,82 @@ module.exports = {
 //       filename: "index.html",
 //     }),
 //     new Dotenv(),
-//     new CleanPlugin.CleanWebpackPlugin(),
-
 //   ],
 
 //   devServer: {
-//     static: {
-//       directory: path.resolve(__dirname, "dist"),
-//     },
+//     // static: {
+//     //   directory: path.resolve(__dirname, "dist"),
+//     // },
+//     static: [
+//       {
+//         directory: path.join(__dirname),
+//       },
+//     ],
 //     compress: true,
 //     port: 9000,
 //     open: true,
 //   },
 
-//   mode: "production",
+//   mode: "development",
 // };
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanPlugin = require("clean-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+  entry: "./src/ts/app.ts",
+
+  output: {
+    filename: "js/bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+
+  devtool: "source-map",
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[name][ext][query]",
+        },
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      filename: "index.html",
+    }),
+    new Dotenv(),
+    new CleanPlugin.CleanWebpackPlugin(),
+  ],
+
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    compress: true,
+    port: 9000,
+    open: true,
+  },
+
+  mode: "production",
+};
